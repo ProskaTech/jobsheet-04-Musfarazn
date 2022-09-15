960cabad9a39c55e7873efc00d3ac4223a174070
Musfarazn <112687844+Musfarazn@users.noreply.github.com>
2022-09-15 15:52:37 +0700

    Add files via upload

---
+++ prak4.java/src/main/java/com/mycompany/prak4/java/PenggunaanOperatorLogikaNOT.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prak4.java;
+
+/**
+ *
+ * @author Musfara
+ */
+public class PenggunaanOperatorLogikaNOT {
+     public static void main(String[] args) {
+        boolean va11 = true;
+        boolean va12 = false;
+        
+        System.out.println(!va11);
+        System.out.println(!va12);
+     }
+}
---
+++ prak4.java/src/main/java/com/mycompany/prak4/java/PenggunaanOperatorTernaryKondisi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prak4.java;
+
+/**
+ *
+ * @author Musfara
+ */
+public class PenggunaanOperatorTernaryKondisi {
+    public static void main(String[] args) {
+        int score = 0;
+        char answer = 'a';
+        
+        score = (answer == 'b') ? 10 : 0;
+        System.out.println("Score = " + score);
+    }
+    
+}
